79682b734d84fb051e96efb9d0c969e3b5870be8
oksean <seanokwodu2@gmail.com>
2022-11-25 01:09:44 -0800

    my first js class

---
+++ main.js
@@ -0,0 +1,57 @@
+let message= prompt('hello world')
+
+let message2=('hello world') 
+console.log(message2)
+let z;
+console.log(z)
+let y= 'null'
+console.log(y)
+// how to use typeof
+let w= 5.02;
+ console.log( typeof (w));
+// declaration of array
+ let r= ['buy', 'try',70, ];
+ console.log(r);
+
+ let obj={id:5, name:'sean',array:['buy','try',36]}
+ console.log(typeof(obj))
+ console.log(obj)
+let num1=10
+let num2=10
+let num=num1+num2
+console.log(num +25)
+console.log(10 +10 )
+
+let question=prompt('please input a number')
+console.log(parseInt(question)+ parseInt(20))
+
+let str="obi"
+console.log(parseInt(str))
+
+let str2=5
+console.log( typeof(str2))
+let cp=str2.toString()
+console.log(cp)
+
+let str3=prompt("please input your name")
+console.log(str3 + " " +"okwodu")
+
+
+let b=5;
+let v="2";
+let num3=b+v
+console.log( typeof(num3))
+
+let ques=prompt("input anything");
+if ( ques>=0){
+    alert(" Hurray!! you have written a number");
+} else if (ques!=0){
+    alert("hurray!! you have written a string"); 
+}
+
+
+ let first=prompt("input first number")
+ let second =prompt("input second number")
+ let third=prompt("input third number")
+ let fourth=prompt("input fourth number")
+alert(parseInt(second) + parseInt(fourth))
